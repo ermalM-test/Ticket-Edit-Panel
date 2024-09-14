18c86a65a72159ce89cc5f703772cff4e913ab2f
ErmalMilazimi <milazimi37@gmail.com>
2024-09-15 00:31:49 +0200

    svg-icon: logic changed

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -2,8 +2,40 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss'],
+  template: `
+    <div class="banner">
+      <svg-icon iconName="icon-logicpilot"></svg-icon>
+      <p>{{ text }}</p>
+    </div>
+  `,
+  styles: [
+    `
+      .banner {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding: 10px;
+        margin: 4px 0;
+        border-radius: 6px;
+        background: rgb(245, 247, 255);
+        background: rgb(242, 244, 255);
+
+        background: linear-gradient(
+          100deg,
+          rgba(242, 244, 255, 1) 0%,
+          rgba(255, 240, 253, 1) 100%
+        );
+        * {
+          line-height: 1;
+        }
+        p {
+          margin: 0;
+          font-size: 14px;
+          color: #687196;
+        }
+      }
+    `,
+  ],
 })
 export class BannerComponent {
   @Input() text: string = '';
--- src/app/svg-icon/svg-icon.component.ts
+++ src/app/svg-icon/svg-icon.component.ts
@@ -5,12 +5,41 @@ import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'svg-icon',
-  templateUrl: './svg-icon.component.html',
-  styles: ['img,svg{vertical-align:auto;line-height:1}'],
+  template: '<div [innerHTML]="sanitizedSvg" class="svg-icon-container"></div>',
+  styles: [
+    '.svg-icon-container{display:flex;align-items:center;justify-content:center} ',
+  ],
+  // templateUrl: './svg-icon.component.html',
+  // styles: ['img,svg{vertical-align:auto;line-height:1}'],
 })
 export class SvgIconComponent {
   @Input() iconName: string = '';
-  @Input() color: string = 'black';
+  @Input() color: string = '#000';
+  @Input() defaultColor: string = '#687196';
   @Input() width: string = '16px';
   @Input() height: string = '16px';
+
+  sanitizedSvg!: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadSvg();
+  }
+
+  loadSvg() {
+    const iconUrl = `assets/icons/${this.iconName}.svg`;
+
+    firstValueFrom(this.http.get(iconUrl, { responseType: 'text' }))
+      .then((svg) => {
+        const coloredSvg = svg.replace(
+          '<svg',
+          `<svg width: ${this.width}; height: ${this.height};"`
+        );
+        this.sanitizedSvg = this.sanitizer.bypassSecurityTrustHtml(coloredSvg);
+      })
+      .catch((err) => {
+        console.error('Error loading SVG:', err);
+      });
+  }
 }
